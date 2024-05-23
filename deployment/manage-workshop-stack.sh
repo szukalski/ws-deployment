e2f0beeb5cbdf9f74ce253bab1bc5abd9fc9131a
EC2 Default User <ec2-user@ip-172-31-16-40.eu-central-1.compute.internal>
2024-05-23 13:53:29 +0000

    Update.

--- deployment/manage-workshop-stack.sh
+++ deployment/manage-workshop-stack.sh
@@ -14,7 +14,98 @@ C9_INSTANCE_PROFILE_PARAMETER_NAME="/"$WORKSHOP_NAME"/Cloud9/InstanceProfileName
 TARGET_USER="ec2-user"
 CDK_C9_STACK="WS-Cloud9Stack"
 
-# Define how to manage your workshop stack
+##  Helper functions
+# Try to run a command 3 times then timeout
+function retry {
+  local n=1
+  local max=3
+  local delay=10
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$max:"
+        sleep $delay;
+      else
+        echo "The command has failed after $n attempts."
+        exit 1
+      fi
+    }
+  done
+}
+
+# Run an SSM command on an EC2 instance
+run_ssm_command() {
+    SSM_COMMAND="$1"
+    parameters=$(jq -n --arg cm "runuser -l \"$TARGET_USER\" -c \"$SSM_COMMAND\"" '{executionTimeout:["3600"], commands: [$cm]}')
+    comment=$(echo "$SSM_COMMAND" | cut -c1-100)
+    # send ssm command to instance id in C9_ID
+    sh_command_id=$(aws ssm send-command \
+        --targets "Key=InstanceIds,Values=$C9_ID" \
+        --document-name "AWS-RunShellScript" \
+        --parameters "$parameters" \
+        --timeout-seconds 3600 \
+        --comment "$comment" \
+        --output text \
+        --query "Command.CommandId")
+
+    command_status="InProgress" # seed status var
+    while [[ "$command_status" == "InProgress" || "$command_status" == "Pending" || "$command_status" == "Delayed" ]]; do
+        sleep 15
+        command_invocation=$(aws ssm get-command-invocation \
+            --command-id "$sh_command_id" \
+            --instance-id "$C9_ID")
+        echo -E "$command_invocation" | jq # for debugging purposes
+        command_status=$(echo -E "$command_invocation" | jq -r '.Status')
+    done
+
+    if [ "$command_status" != "Success" ]; then
+        echo "failed executing $SSM_COMMAND : $command_status" && exit 1
+    else
+        echo "successfully completed execution!"
+    fi
+}
+
+# Wait for an EC2 instance to become available and for it to be online in SSM
+wait_for_instance_ssm() {
+    INSTANCE_ID="$1"
+    echo "Waiting for instance $INSTANCE_ID to become available"
+    aws ec2 wait instance-status-ok --instance-ids "$INSTANCE_ID"
+    echo "Instance $INSTANCE_ID is available"
+    ssm_status=$(aws ssm describe-instance-information --filters "Key=InstanceIds,Values=$INSTANCE_ID" --query 'InstanceInformationList[].PingStatus' --output text)
+    while [[ "$ssm_status" != "Online" ]]; do
+        echo "Instance $INSTANCE_ID is not online in SSM yet. Waiting 15 seconds"
+        sleep 15
+        ssm_status=$(aws ssm describe-instance-information --filters "Key=InstanceIds, Values=$INSTANCE_ID" --query 'InstanceInformationList[].PingStatus' --output text)
+    done
+    echo "Instance $INSTANCE_ID is online in SSM"
+}
+
+# Replace instance profile on an EC2 instance
+replace_instance_profile() {
+    echo "Replacing instance profile"
+    association_id=$(aws ec2 describe-iam-instance-profile-associations --filter "Name=instance-id,Values=$C9_ID" --query 'IamInstanceProfileAssociations[].AssociationId' --output text)
+    if [ ! association_id == "" ]; then
+        aws ec2 disassociate-iam-instance-profile --association-id $association_id
+        command_status=$(aws ec2 describe-iam-instance-profile-associations --filter "Name=instance-id,Values=$C9_ID" --query 'IamInstanceProfileAssociations[].State' --output text)
+        while [[ "$command_status" == "disassociating" ]]; do
+            sleep 15
+            command_status=$(aws ec2 describe-iam-instance-profile-associations --filter "Name=instance-id,Values=$C9_ID" --query 'IamInstanceProfileAssociations[].State' --output text)
+        done
+    fi
+    aws ec2 associate-iam-instance-profile --instance-id $C9_ID --iam-instance-profile Name=$C9_INSTANCE_PROFILE_NAME
+    command_status=$(aws ec2 describe-iam-instance-profile-associations --filter "Name=instance-id,Values=$C9_ID" --query 'IamInstanceProfileAssociations[].State' --output text)
+    while [[ "$command_status" == "associating" ]]; do
+        sleep 15
+        command_status=$(aws ec2 describe-iam-instance-profile-associations --filter "Name=instance-id,Values=$C9_ID" --query 'IamInstanceProfileAssociations[].State' --output text)
+    done
+    echo "Instance profile replaced. Rebooting instance"
+    aws ec2 reboot-instances --instance-ids "$C9_ID"
+    wait_for_instance_ssm "$C9_ID"
+    echo "Instance rebooted"
+}
+
+## Define how to manage your workshop stack
 # The example below deploys a Cloud9 instance and uses it to bootstrap a workshop.
 manage_workshop_stack() {
     STACK_OPERATION=$(echo "$1" | tr '[:upper:]' '[:lower:]')
